c345d7f1f4d42d18124d6998a8a5cef64200a268
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-04-20 15:27:42 +0200

    end

---
+++ script.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        titolo : "Welcome on",
+        img: {
+            "immagine": "https://www.vectorlogo.zone/logos/vuejs/vuejs-ar21.svg"
+        }
+    }
+
+})
